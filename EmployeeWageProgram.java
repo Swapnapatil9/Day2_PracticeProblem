df37fd162344bece73850f1162d002a674454f7a
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-04-05 15:23:25 +0530

    Added: Using Switch case to calculate Employee Daily Wage

--- EmployeeWageProgram.java
+++ EmployeeWageProgram.java
@@ -3,22 +3,25 @@ public class EmployeeWageProgram{
 	public static void main(String[] args){
 	System.out.println("Welcome to Employee Wage Computation program");
 	
-	//UC3
+	//UC4
 	int IS_FULL_TIME = 1;
 	int IS_PART_TIME = 2;
 	int WAGE_PER_HOUR = 20;
 	int empHrs = 0;
 	int empWage = 0;
 	int empCheck = (int)Math.floor(Math.random()*10)%3;
-	if(empCheck == IS_FULL_TIME){
+	switch(empCheck){
+	case 1:
 		System.out.println("Employee is Full-time");
 		empHrs = 8;
-	}
-	else if(empCheck == IS_PART_TIME){
+		break;
+		
+	case 2:
 		System.out.println("Employee is Part-time");
 		empHrs = 4;
-	}
-	else{
+		break;
+		
+	default:
 		System.out.println("Employee is Absent");
 		empHrs = 0;
 	}
