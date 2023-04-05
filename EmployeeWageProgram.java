9015a89d3fca363d523229b820528b8439dafc9f
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-04-05 14:51:24 +0530

    Added: Calculate Part Time Employee Daily Wage

--- EmployeeWageProgram.java
+++ EmployeeWageProgram.java
@@ -3,16 +3,22 @@ public class EmployeeWageProgram{
 	public static void main(String[] args){
 	System.out.println("Welcome to Employee Wage Computation program");
 	
-	//UC2
+	//UC3
 	int IS_FULL_TIME = 1;
+	int IS_PART_TIME = 2;
 	int WAGE_PER_HOUR = 20;
 	int empHrs = 0;
 	int empWage = 0;
-	int empCheck = (int)Math.floor(Math.random()*10)%2;
+	int empCheck = (int)Math.floor(Math.random()*10)%3;
 	if(empCheck == IS_FULL_TIME){
-		System.out.println("Employee is Present");
+		System.out.println("Employee is Full-time");
 		empHrs = 8;
-	}else{
+	}
+	else if(empCheck == IS_PART_TIME){
+		System.out.println("Employee is Part-time");
+		empHrs = 4;
+	}
+	else{
 		System.out.println("Employee is Absent");
 		empHrs = 0;
 	}
